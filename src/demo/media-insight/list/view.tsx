5c987e3184ba2c18bafb72067191856f0e945623
thejskhan <thejargalsaikhan@gmail.com>
2024-10-22 01:45:12 +0800

    fixes

--- src/demo/media-insight/list/view.tsx
+++ src/demo/media-insight/list/view.tsx
@@ -39,7 +39,7 @@ import {
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog'
-import PPT from '../detail/components/ppt'
+import PPT from '@/demo/common/components/ppt'
 
 export type TRow = {
   id: string
